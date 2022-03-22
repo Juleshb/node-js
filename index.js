10c2113f213ca50898442a94d8dd1451ad0e29a4
Juleshb <habaruremajules@gmail.com>
2022-03-22 09:47:28 +0200

    comfig cros

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors";
 import userRouter from "./routes/userRoutes.js"
 import messageRouter from "./routes/messageRoutes.js"
 import blogRouter from "./routes/blogRoutes.js"
@@ -6,6 +7,7 @@ import commentRouter from "./routes/commentRoutes.js"
 import swaggertest from "./swagger/doc.js"
 import { login } from './controllers/userController.js';
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1',swaggertest)
 app.get('/',(req,res)=>res.json({message:'request received, thanks then'}));
