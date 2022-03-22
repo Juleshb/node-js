57714976aceeeaa237bc1b5000ded4f8e9f08323
Juleshb <habaruremajules@gmail.com>
2022-03-22 19:27:15 +0200

    remove required post

--- models/blogModel.js
+++ models/blogModel.js
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 const blogSchema=new mongoose.Schema({
     title:{
         type:String,
-        required:[true,"this field is required"]
+        //required:[true,"this field is required"]
         
     },
     description:{
         type:String,
-        required:[true,"this field is required"],
+       // required:[true,"this field is required"],
         trim:true
     }, body:{
         type:String,
-        required:[true,"this field is required"]
+        //required:[true,"this field is required"]
         
     }, photo:{
         type:String,
-        required:[true,"this field is required"]
+       // required:[true,"this field is required"]
         
     },
     comments:[{
