ead2f09dcbe1fbead3a71f552198700429b88a58
Juleshb <habaruremajules@gmail.com>
2022-03-23 17:48:44 +0200

    Back end With font end

---
+++ font end/Dashboard/auth.js
@@ -0,0 +1,18 @@
+
+function isLoginedIn()
+{
+    let token = localStorage.getItem("token");
+    if(token==null)
+    {
+        location.href = '/login.html'
+    }
+}
+isLoginedIn()
+
+ function logout()
+{
+  localStorage.clear();
+  window.location.reload();
+}
+
+document.getElementById("logoutBtn").addEventListener("click",ev=>logout())
---
+++ font end/Dashboard/stop.js
@@ -0,0 +1,11 @@
+function logout() {
+    auth.signOut().then(res=>{
+        auth.onAuthStateChanged(user => {
+            if (user) {
+
+            } else {
+                location.href = "/login.html"
+            }
+        })
+    })
+}
